54d2b33dee92eb79073c41b2fea6840e6f939781
Hamid Reza Salimian <xsx.hamid@gmail.com>
2022-04-19 14:06:13 +0430

    some changes of  Account class + tsconfig

--- src/actions/account/AccountClass.ts
+++ src/actions/account/AccountClass.ts
@@ -3,6 +3,12 @@ import {BaseActionClass}             from '../BaseActionClass';
 
 import LoginAction                   from './login';
 import type { loginActionInput }     from '../../types/loginAction';
+import type { registerActionInput }     from '../../types/registerAction';
+import type { editPriofileActionInput  }     from '../../types/editProfileAction';
+import type { reviewHomeActionInput  }     from '../../types/reviewHomeAction';
+import RegisterAction from './register';
+import EditProfileAction from './editProfile';
+import ReviewHome from './reviewHome';
 
 export default class Account extends BaseActionClass{
 
@@ -16,8 +22,41 @@ export default class Account extends BaseActionClass{
         let browser = this.browser;
         let page    = this.page;
         let login   = new LoginAction(input);
-        login.setBrowser(bot,browser,page);
+        login.setup(bot,browser,page);
         await login.start();
+        return this;
+    }
+    
+    public async register(input: registerActionInput){
+
+        let bot         = this.bot;
+        let browser     = this.browser;
+        let page        = this.page;
+        let register    = new RegisterAction(input)
+        register.setup(bot,browser,page);
+        await register.start();
+        return this;
+    }
+    
+    public async editProfile(input: editPriofileActionInput){
+
+        let bot         = this.bot;
+        let browser     = this.browser;
+        let page        = this.page;
+        let editProfile = new EditProfileAction(input)
+        editProfile.setup(bot,browser,page);
+        await editProfile.start();
+        return this;
+    }
+    
+    public async reviewHome(input: reviewHomeActionInput){
+
+        let bot         = this.bot;
+        let browser     = this.browser;
+        let page        = this.page;
+        let reviewHome  = new ReviewHome(input)
+        reviewHome.setup(bot,browser,page);
+        await reviewHome.start();
         return this;
     }
 
